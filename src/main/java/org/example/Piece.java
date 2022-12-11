aa61e57383f2684f848ca112b843bb9cacf6a66d
progManu <41389275+progManu@users.noreply.github.com>
2022-12-11 10:43:03 +0100

    Adding equals method Piece.java and testing equals method in PieceTest.java

--- src/main/java/org/example/Piece.java
+++ src/main/java/org/example/Piece.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.security.SecureRandom;
+import java.util.Objects;
+
 public class Piece {
 
 //shape of the piece
@@ -15,16 +17,37 @@ public class Piece {
     private static final Tile[][] E={{new Tile(true),new Tile(true),new Tile(true),new Tile(true),new Tile(true)}};
 
     // square pieces
-    private static final Tile[][] F={{new Tile(true),new Tile(true)},{new Tile(true),new Tile(true)}};
-    private static final Tile[][] G={{new Tile(true),new Tile(true),new Tile(true)},{new Tile(true), new Tile(true), new Tile(true)}, {new Tile(true),new Tile(true), new Tile(true)}};
+    private static final Tile[][] F={
+            {new Tile(true),new Tile(true)},
+            {new Tile(true),new Tile(true)}
+    };
+    private static final Tile[][] G={
+            {new Tile(true),new Tile(true),new Tile(true)},
+            {new Tile(true), new Tile(true), new Tile(true)},
+            {new Tile(true),new Tile(true), new Tile(true)}
+    };
 
     //'L' pieces
-    private static final Tile[][] H={{new Tile(true), new Tile()}, {new Tile(true), new Tile(true)}};
-    private static final Tile[][] I={{new Tile(true),new Tile(),new Tile()}, {new Tile(true),new Tile(),new Tile()}, {new Tile(true),new Tile(true),new Tile(true)} };
+    private static final Tile[][] H={
+            {new Tile(true), new Tile()},
+            {new Tile(true), new Tile(true)}
+    };
+    private static final Tile[][] I={
+            {new Tile(true),new Tile(),new Tile()},
+            {new Tile(true),new Tile(),new Tile()},
+            {new Tile(true),new Tile(true),new Tile(true)}
+    };
 
     //reversed 'L' pieces
-    private static final Tile[][] J={{new Tile(), new Tile(true)}, {new Tile(true), new Tile(true)}};
-    private static final Tile[][] K={{new Tile(),new Tile(),new Tile(true)}, {new Tile(),new Tile(),new Tile(true)}, {new Tile(true),new Tile(true),new Tile(true)} };
+    private static final Tile[][] J={
+            {new Tile(), new Tile(true)},
+            {new Tile(true), new Tile(true)}
+    };
+    private static final Tile[][] K={
+            {new Tile(),new Tile(),new Tile(true)},
+            {new Tile(),new Tile(),new Tile(true)},
+            {new Tile(true),new Tile(true),new Tile(true)}
+    };
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -44,6 +67,33 @@ public class Piece {
             throw new IllegalArgumentException("Piece Index out of bounds");
 
         this.shape = possibleShapes[pieceIndex];
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        Piece piece = (Piece) o;
+
+        if(piece.shape[0].length != this.shape[0].length ||
+                piece.shape.length != this.shape.length)
+            return false;
+
+        boolean result = true;
+
+        for(int i = 0; i < this.shape[0].length; i++)
+        {
+            for(int j = 0; j < this.shape.length; j++)
+            {
+                result &= this.shape[j][i].equals(piece.shape[j][i]);
+            }
+        }
+        // field comparison
+        return result;
     }
 
 }
--- src/test/java/PieceTest.java
+++ src/test/java/PieceTest.java
@@ -1,7 +1,13 @@
 import org.example.Piece;
+import org.example.Tile;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PieceTest {
@@ -11,5 +17,21 @@ public class PieceTest {
         assertThrows(IllegalArgumentException.class,
                 () -> new Piece(pieceIndex)
         );
+    }
+
+    private static Stream<Arguments> providePiecesToTest() {
+        Piece piece = new Piece(0);
+        return Stream.of(
+                Arguments.of(new Piece(0).equals(piece), true),
+                Arguments.of(new Piece(1).equals(piece), false),
+                Arguments.of(piece.equals(piece), true),
+                Arguments.of(piece.equals(null), false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("providePiecesToTest")
+    void checkEqualPieces(boolean eval, boolean expected){
+        assertEquals(expected, eval);
     }
 }
