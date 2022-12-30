3de14d32bc50be48c85cb3b56fbfe9e3c47cc076
Johnny <giovannizanin00@gmail.com>
2022-12-30 13:17:42 +0100

    prima rappresentazione board di gioco

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,4 +1,32 @@
 package org.example;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Game {
-}
+    public JFrame gameFrame;
+    public Board Board;
+
+    public Game() {
+        this.gameFrame = new JFrame("1010");
+        this.Board = new Board();
+
+        this.gameFrame.setSize(700, 700);
+        this.gameFrame.setResizable(true);
+        this.gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel p = new JPanel() {
+            //per rappresentare figure è sufficiente fare override del metodo paint di JPanel
+            @Override
+            public void paint(Graphics gr) {
+                for (int i = 0; i < Board.gameBoard.length; i++) {
+                    for (int j = 0; j < Board.gameBoard[i].length; j++) {
+                        gr.setColor(Board.gameBoard[i][j].getColor());
+                        gr.fillRect(20 + 40 * i, 20 + 40 * j, 39, 39);
+                    }
+                }
+            }
+        };
+        this.gameFrame.add(p);
+        }
+    }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,14 @@
 package org.example;
 
-public class Main {
 
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.gameFrame.setVisible(true);
+
+
+        //per modificare la board bsta cambiare colore ai tile di game.board.gamerBoard
+        // bisogna rappresentare i pezzi e creare spazi cliccabili in corrispondenza di pezzi e caselle della scacchiera
+    }
 }
