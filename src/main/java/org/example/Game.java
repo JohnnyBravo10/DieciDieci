6f227ede75598e2ed39928dce7e25e772bcd3f57
Johnny <giovannizanin00@gmail.com>
2023-01-25 11:06:17 +0100

    aggiunta grafica tasto rotazione

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -3,10 +3,12 @@ package org.example;
 import javax.swing.*;
 import java.awt.*;
 
+
 public class Game {
     public JFrame gameFrame;
     private Board Board; //board di gioco
 
+    public JButton rotationButton; //bottone per ruotare i pezzi
     private Piece[] availablePieces; //tre pezzi disponibili da piazzare
 
     public Game() {
@@ -14,10 +16,11 @@ public class Game {
         this.Board = new Board();
         this.availablePieces=new Piece[]{PieceSet.getRandomPiece(),PieceSet.getRandomPiece(),PieceSet.getRandomPiece()};
 
-        this.gameFrame.setSize(1000, 800);
+        this.gameFrame.setSize(1200, 800);
         this.gameFrame.setResizable(true);
         this.gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        //in JPanel p sono disegnate board e pezzi disponibili
         JPanel p = new JPanel() {
             //per rappresentare figure è sufficiente fare override del metodo paint di JPanel
             //qui vengono disegnata board e tre pezzi, c'è da capire come fare a refreshare il jframe quando Board viene modificata o available pieces viene cambiato
@@ -35,11 +38,21 @@ public class Game {
                 for (int l=0; l<availablePieces.length; l++){//per ognuno dei tre pezzi
                     gr.setColor(availablePieces[l].getPieceColor());//seleziono il colore del pezzo
                     for (int k=0; k<availablePieces[l].getPieceGeometry().getShape().getRowDimension(); k++){//per ogni quadratino del pezzo
-                        gr.fillRect((int)(120/*offset da sinistra*/+230*l/*distanza tra i punti (0,0) dei pezzi*/+ 40*availablePieces[l].getPieceGeometry().getShape().getRow(k)[0]/*ascissa del quadratino*/), (int)(550 /*offset dall'alto del (0,0) dei pezzi*/-40*availablePieces[l].getPieceGeometry().getShape().getRow(k)[1]/*ascissa del quadratino*/), 39,39);
+                        gr.fillRect((int)(120/*offset da sinistra*/+250*l/*distanza tra i punti (0,0) dei pezzi*/+ 40*availablePieces[l].getPieceGeometry().getShape().getRow(k)[0]/*ascissa del quadratino*/), (int)(550 /*offset dall'alto del (0,0) dei pezzi*/-40*availablePieces[l].getPieceGeometry().getShape().getRow(k)[1]/*ascissa del quadratino*/), 39,39);
                     }
                 }
             }
         };
+
+        //in JPAnel q c'è il tasto per ruotare
+        this.rotationButton= new JButton("Rotate");
+        JPanel q= new JPanel();
+        q.add(this.rotationButton);
+        q.setBounds(800,550,200,200);
+        this.gameFrame.add(q);
+
+        p.setBounds(0,0,1000,800);
         this.gameFrame.add(p);
-        }
+
+    }
     }
