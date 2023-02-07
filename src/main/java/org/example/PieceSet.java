6730b71a6f7644456a7611f9560f5bd1091de200
progManu <41389275+progManu@users.noreply.github.com>
2023-02-07 12:01:05 +0100

    Changed how to deal with piece coordinates

--- src/main/java/org/example/PieceSet.java
+++ src/main/java/org/example/PieceSet.java
@@ -9,33 +9,34 @@ import java.util.Random;
 public class PieceSet {
     private static PieceSet set;
 
-    //ho reso possibleSet un campo static final perché alla fine è una costante. Tenendo così
-    //non dobbiamo più usare un costruttore che costruisce il possibleSet (i prof fanno così nelle cell dell'esempio della banca)
+    // ho reso possibleSet un campo static final perché alla fine è una costante. Tenendo così
+    // non dobbiamo più usare un costruttore che costruisce il possibleSet (i prof fanno così nelle cell dell'esempio della banca)
     // ho modificato anche i test. ora non c'è neanche bisogno di istanziare la classe pieceset, secondo me così è meglio in caso ditemi
-    //se non vi convince. (poi volendo tutt questo campo final si potrebbe addiritture mettere nella classe piece e cancellare la class pieceset
+    // se non vi convince. (poi volendo tutt questo campo final si potrebbe addiritture mettere nella classe piece e cancellare la class pieceset
     // in alternativa si potrebbe anche pensare di rendere pieceset una classe record :o
-    private static final Piece[] possibleSet=new Piece[]{
+
+    private static final Piece[] possibleSet = new Piece[]{
             // STRAIGTH
             new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{{0, 0}})),
                     Piece.ShapeType.STRAIGHT1x,
                     Color.ORANGE),
-            new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{{0, 0}, {1, 0}})),
+            new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{{-0.5, 0}, {0.5, 0}})),
                     Piece.ShapeType.STRAIGHT2x,
                     Color.BLUE),
-            new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{{0, 0}, {1, 0}, {2, 0}})),
+            new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{{-1, 0}, {0, 0}, {1, 0}})),
                     Piece.ShapeType.STRAIGHT3x,
                     Color.RED),
-            new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{{0, 0}, {1, 0}, {2, 0}, {3, 0}})),
+            new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{{-1, 0}, {-0.5, 0}, {0.5, 0}, {1, 0}})),
                     Piece.ShapeType.STRAIGHT4x,
                     Color.CYAN),
-            new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}})),
+            new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{{-2, 0}, {-1, 0}, {0, 0}, {1, 0}, {2, 0}})),
                     Piece.ShapeType.STRAIGHT5x,
                     Color.GREEN),
 
             // SQUARES
             new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{
-                    {0, 0}, {0, 1},
-                    {1, 0}, {1, 1}})),
+                    {-0.5, -0.5}, {-0.5, 0.5},
+                    {0.5, 0.5}, {0.5, -0.5}})),
                     Piece.ShapeType.SQUARE2x,
                     Color.PINK),
             new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{
@@ -46,80 +47,15 @@ public class PieceSet {
                     Color.DARK_GRAY),
             //ELLES
             new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{
-                    {0, 0}, {0, 1}, {1, 0}})),
+                    {-0.5, -0.5}, {-0.5, 0.5}, {0.5, -0.5}})),
                     Piece.ShapeType.ELLE2x,
                     Color.MAGENTA),
             new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{
-                    {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}})),
+                    {0, 0}, {1, 0}, {2, 0}, {0, 1}, {0, 2}})),
                     Piece.ShapeType.ELLE3x,
-                    Color.YELLOW),
-
-            //REVERSED ELLES "Γ"
-
-            new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{
-                    {0, 0}, {0, -1}, {1, 0}})),
-                    Piece.ShapeType.REVERSED_ELLE2X,
-                    Color.LIGHT_GRAY),
-            new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{
-                    {-1, 1}, {-1, 0}, {-1, -1}, {0, 1}, {1, 1}})),
-                    Piece.ShapeType.REVERSED_ELLE3X,
-                    Color.BLACK),
+                    Color.YELLOW)
     };
     private static final Random gen = new Random();
-
-    /*
-    private PieceSet(){
-        this.possibleSet = new Piece[]{
-                // STRAIGTH
-                new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{{0, 0}, {1, 0}})),
-                        Piece.ShapeType.STRAIGHT2x,
-                        Color.BLUE),
-                new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{{0, 0}, {1, 0}, {2, 0}})),
-                        Piece.ShapeType.STRAIGHT3x,
-                        Color.RED),
-                new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{{0, 0}, {1, 0}, {2, 0}, {3, 0}})),
-                        Piece.ShapeType.STRAIGHT4x,
-                        Color.CYAN),
-                new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}})),
-                        Piece.ShapeType.STRAIGHT5x,
-                        Color.GREEN),
-
-                // SQUARES
-                new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{
-                        {0, 0}, {0, 1},
-                        {1, 0}, {1, 1}})),
-                        Piece.ShapeType.SQUARE2x,
-                        Color.PINK),
-                new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{
-                        {0, 0}, {0, 1}, {0, -1},
-                        {1, 0}, {1, 1}, {1, -1},
-                        {-1, 0}, {-1, 1}, {-1, -1}})),
-                        Piece.ShapeType.SQUARE3x,
-                        Color.DARK_GRAY),
-                //ELLES
-                new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{
-                        {0, 0}, {0, 1}, {1, 0}})),
-                        Piece.ShapeType.ELLE2x,
-                        Color.MAGENTA),
-                new Piece(new PieceGeometry(new Array2DRowRealMatrix(new double[][]{
-                        {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}})),
-                        Piece.ShapeType.ELLE3x,
-                        Color.YELLOW)
-        };
-    }
-    */
-
-
-    /*
-    public static PieceSet getInstance(){
-        if(set == null)
-        {
-            set = new PieceSet();
-        }
-
-        return set;
-    }
-    */
 
 
     //anche questi due metodi li ho resi statici
